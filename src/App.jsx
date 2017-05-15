6a5dc297dfbba5161a9e941504b974a3ff6f56fc
lmccai1 <lucas.mccain@ampf.com>
2017-05-14 22:03:22 -0500

    begin nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './Home';
 import About from './About';
+import Home from './Home';
+import Nav from './Nav';
 import Post from './Post';
 import Posts from './Posts';
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/nav" component={Nav} />
     <Route exact path="/about" component={About} />
     <Route exact path="/posts/" component={Posts} />
     <Route path="/posts/:number" component={Post} />
